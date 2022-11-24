45ebdf6781068fa3d7b99ac2cbe21ca59862596e
summer <xiayansummer@msn.com>
2022-11-24 18:19:38 +0800

    models update

--- source/models.py
+++ source/models.py
@@ -30,3 +30,29 @@ class BDStatus(models.Model):
     def save(self, *args, **kwargs):
         super(BDStatus, self).save(*args, **kwargs)
 
+class Familiarlevel(models.Model):
+    name = models.CharField(max_length=24, blank=True)
+    score = models.IntegerField(blank=True)
+
+    def save(self, *args, **kwargs):
+        super(Familiarlevel, self).save(*args, **kwargs)
+
+class Industry(models.Model):
+    PindustryID = models.IntegerField()
+    name = models.CharField(max_length=24, blank=True)
+    bucket = models.CharField(max_length=12, blank=True)
+    key = models.CharField(max_length=128, blank=True)
+    datasource = models.ForeignKey(DataSource, on_delete=models.CASCADE, blank=True)
+
+    def save(self, *args, **kwargs):
+        super(Industry, self).save(*args, **kwargs)
+
+class Group(models.Model):
+    name = models.CharField(max_length=24, blank=True)
+    shareInvestor = models.BooleanField(default=False)
+    manager = models.BigIntegerField(blank=True)
+    ongongingurl = models.CharField(max_length=128, blank=True)
+    datasource = models.ForeignKey(DataSource, on_delete=models.CASCADE, blank=True)
+
+    def save(self, *args, **kwargs):
+        super(Group, self).save(*args, **kwargs)
